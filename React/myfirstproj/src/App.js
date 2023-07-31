9d0912760dd3f4b93683c70449fd95b5daa8028b
sam2891 <dwsnsamuel@gmail.com>
2023-07-31 22:54:35 +0530

    commit

--- React/myfirstproj/src/App.js
+++ React/myfirstproj/src/App.js
@@ -1,22 +1,12 @@
 
 import './App.css';
+import Home from './component/Home ';
 
 function App() {
 
   return (
     <div className="Sam">
-      <h1>My First React App</h1>
-
-      <h2>About Me:</h2>
-      <div>
-        <img src="https://picsum.photos/200/300" alt="random"></img>
-      </div>
-      <p>
-        svyystvynaptvmypatvmitc,povohxgk.hgs;rhg;hre;wonbononb;on;obn;jn;osrn:ONbno"Obn;ronzobn[rfndmon
-        bdvsbivbpipionvdpiobnpsojBNOPNBO:NBPBpoNBPONBponob<br></br>
-        vsbibvibvdisbivbibvpibvibibnibnfanbnlbn;zfbn;bhnbcijhanfbpnfobonpbnpbnzpbiznabpainbPIONBPnbpnbfpbfn
-        seagingnhnaphondponmpoketobm....
-      </p>
+      <Home />
 
 
     </div>
---
+++ React/myfirstproj/src/component/Home .js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h1>My First React App</h1>
+
+            <h2>About Me:</h2>
+            <div>
+                <img src="https://picsum.photos/200/300" alt="random"></img>
+            </div>
+            <p>
+                svyystvynaptvmypatvmitc,povohxgk.hgs;rhg;hre;wonbononb;on;obn;jn;osrn:ONbno"Obn;ronzobn[rfndmon
+                bdvsbivbpipionvdpiobnpsojBNOPNBO:NBPBpoNBPONBponob<br></br>
+                vsbibvibvdisbivbibvpibvibibnibnfanbnlbn;zfbn;bhnbcijhanfbpnfobonpbnpbnzpbiznabpainbPIONBPnbpnbfpbfn
+                seagingnhnaphondponmpoketobm....
+            </p>
+
+
+        </div>
+    )
+}
+
+export default Home 
