9d5a289a70f1b3f0d174cbd976385e60ea70c5e4
sam2891 <dwsnsamuel@gmail.com>
2023-08-04 16:59:54 +0530

    react File

--- React/myfirstproj/src/App.js
+++ React/myfirstproj/src/App.js
@@ -4,10 +4,21 @@ import Home from './component/Home ';
 
 function App() {
   const heading = "My First React Application"
+  // eslint-disable-next-line no-unused-vars
+  const fruits = ["Apple","Banana","Orange","Mango"]
 
   return (
     <div className="Sam">
       <Home title={heading} />
+      <h1>My Fruits</h1>
+      <ul>
+        {    //java script canbe written in HTml with curly brackets 
+
+        fruits.map((fruits,index)=> (
+        <li key= {index}>{fruits}</li>
+         ))
+         }
+       </ul>
 
 
     </div>
